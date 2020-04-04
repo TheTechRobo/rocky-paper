09fb18247979d9252edc40010fb726e5b5ccf083
Retals <thetechrobo@localhost-localdomain>
2020-04-04 17:25:02 -0400

    Bugs fixed.

--- rockypaper.py
+++ rockypaper.py
@@ -6,29 +6,29 @@ player = input("Rock, paper, or scissors? ")
 player = player.lower()
 def ask(robo):
     if player == "rock":
-        if robo == "0":
+        if robo == 0:
             print("Tie game!")
-        elif robo == "1":
+        elif robo == 1:
             print("The computer won :(")
-        elif robo == "2":
+        elif robo == 2:
             print("You Win! Nice job, you shattered the computer.")
         else:
             print("An error occured while parsing the move.")
     elif player == "paper":
-        if robo == "0":
+        if robo == 0:
             print("You Win! Nice job, you covered the computer. \nHey look, it's overheating!")
-        elif robo == "1":
+        elif robo == 1:
             print("Tie game!")
-        elif robo == "2":
+        elif robo == 2:
             print("The computer won :(")
         else:
             print("An error occured while parsing the move.")
     elif player == "scissors":
-        if robo == "0":
+        if robo == 0:
             print("The computer won :(")
-        elif robo == "1":
+        elif robo == 1:
             print("You win! Nice job, you cut the computer. \nNot sure how you cut metal, but I'll still say you won.")
-        elif robo == "2":
+        elif robo == 2:
             print("Tie game!")
         else:
             print("An error occured while parsing the move.")
