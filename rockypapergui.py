827b66fe4fceae3fdd30d3447d073f4427150a65
Ittussarom Retals Mail Ynohtna <52163910+TheTechRobo@users.noreply.github.com>
2020-05-12 13:40:24 -0400

    omg how many commits?!

--- rockypapergui.py
+++ rockypapergui.py
@@ -47,3 +47,4 @@ def ask(robo, player):
             gui.messagebox.showerror("Error!", "An error occured while parsing the move.")
     else:
         gui.messagebox.showerror("I am mad at you", "You did not choose a valid move.")
+rockypaper.mainloop()
