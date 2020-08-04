c93bf3612716b2d07dcd1f73e2af5698ea5706ae
Ittussarom Retals Mail Ynohtna <52163910+TheTechRobo@users.noreply.github.com>
2020-08-04 13:42:41 -0400

    Add support for GUI version.

--- rockypaperstartup.py
+++ rockypaperstartup.py
@@ -1,9 +1,22 @@
 from runpy import run_path as r
 from sys import exit as e
 try:
+    choice = input("Select an option:\n    1 - Command-line version\n    2 - Graphical version\n>:")
+except KeyboardInterrupt:
+    print("\rExited.")
+except EOFError:
+    print("Task failed successfully.")
+if choice == "1":
+    path = "rockypaper.py"
+elif choice == "2":
+    path = "rockypapergui.py"
+else:
+    print("Defaulting to command-line edition.")
+    path = "rockypaper.py"
+try:
     while True:
-        r(path_name="rockypaper.py")
-except (KeyboardInterrupt, EOFError): #if ^C or ^D
+        r(path_name=path)
+except (KeyboardInterrupt, EOFError): #if ^C
     e("\nSuccessfully exited rockyPaper.")
-except: #any other error
-    e("An unknown error occured :/")
+except Exception as ename: #any other error
+    e("\nAn unknown error occured (%s) :/" % ename)
